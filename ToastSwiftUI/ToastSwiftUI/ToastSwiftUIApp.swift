b1f24f084b29c139ccad66adf60be0554fff8a79
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-06 12:09:46 +0100

    Create project

---
+++ ToastSwiftUI/ToastSwiftUI/ToastSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastSwiftUIApp.swift
+//  ToastSwiftUI
+//
+//  Created by Jesus Antonio Gil on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ToastSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

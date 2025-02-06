8905bbd23f87336710a64ae9655d7de833aedefe
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-06 12:12:31 +0100

    ToastType done

---
+++ ToastSwiftUI/ToastSwiftUI/ToastType.swift
@@ -0,0 +1,8 @@
+//
+//  ToastType.swift
+//  ToastSwiftUI
+//
+//  Created by Jesus Antonio Gil on 6/2/25.
+//
+
+import Foundation
---
+++ ToastSwiftUI/ToastSwiftUI/ToastView.swift
@@ -0,0 +1,18 @@
+//
+//  ToastView.swift
+//  ToastSwiftUI
+//
+//  Created by Jesus Antonio Gil on 6/2/25.
+//
+
+import SwiftUI
+
+struct ToastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToastView()
+}
